2ea36ffa47f2e833bb1544b4de78b397bc7bf21b
Kyle Johnson <11kyle.johnson@gmail.com>
2023-04-20 20:24:43 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import InteractiveRatingCard from "./components/interactive-rating"
+
+function App(): JSX.Element {
+
+  return (
+    <div className="grid place-content-center min-h-screen">
+      <InteractiveRatingCard />
+    </div>
+  )
+}
+
+export default App
